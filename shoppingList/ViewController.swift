091b11cab9575490afa6e4960e4bcf0ae0e685b2
Samuel Schuffler <sschuffler1332@jh-mba158-22.local>
2019-02-08 14:56:18 -0600

    Initial Commit

---
+++ shoppingList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shoppingList
+//
+//  Created by Samuel Schuffler on 2/8/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
